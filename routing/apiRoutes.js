9b6911654210bda17191fe2a9fc0cbcffa81c935
martucazpo <martucazpo@gmail.com>
2019-07-30 22:09:51 -0700

    making sure changes made to master

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,9 +2,8 @@
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-var friendsData = require("../data/friendsData");
+// ==============================================================================
+var friends = require("../app/data/friends");
 
 
 // ===============================================================================
@@ -19,7 +18,7 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
+    res.json(friends);
   });
 
 
@@ -35,7 +34,7 @@ module.exports = function(app) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-  
+    friends.push(req.body);
   });
 
 
